c764f18bdb3bbaa6a00588102b2dba0aa987d631
hiro-orbital <hiro.psp@hotmail.co.jp>
2017-07-28 23:03:03 +0900

    利用可否判定

--- app/src/main/java/com/example/tomoaki/maptest/AreaCheck.java
+++ app/src/main/java/com/example/tomoaki/maptest/AreaCheck.java
@@ -5,11 +5,21 @@ package com.example.tomoaki.maptest;
  */
 
 public class AreaCheck {
-    public boolean AreaCheck(float ido, float keido){
+    float ido_up;
+    float ido_low;
+    float keido_up;
+    float keido_low;
+    AreaCheck(){
+        ido_up = 36.326308f;
+        ido_low = 36.321588f;
+        keido_up = 139.012157f;
+        keido_low = 139.002510f;
+    }
+    public boolean check(double ido, double keido){
         boolean CheckResult;
 
         //1つ目のエリア判定
-        if (ido < 36.326308 && ido >36.321588 && keido < 139.012157 && keido > 139.002510)
+        if (ido < ido_up && ido >ido_low && keido < keido_up && keido > keido_low)
             CheckResult = true;
             else{
                 if (ido <36.3273144 && ido >36.326308 && keido <139.0068387 && keido >139.0066483)
@@ -17,7 +27,7 @@ public class AreaCheck {
                 else
                     CheckResult = false;
             }
-            
+
         return CheckResult;
 
     }
