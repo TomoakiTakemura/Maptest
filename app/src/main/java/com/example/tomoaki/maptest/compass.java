0b19ac9552b18acf6961c63d19d748339a9e250d
hiro-orbital <hiro.psp@hotmail.co.jp>
2017-07-28 15:52:05 +0900

    compass test

---
+++ app/src/main/java/com/example/tomoaki/maptest/compass.java
@@ -0,0 +1,33 @@
+package com.example.tomoaki.maptest;
+
+import android.util.Log;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.support.v4.app.NavUtils;
+
+
+//public class compass extends Activity implements SensorEventListener{
+//    private SensorManager mSensorManager;
+
+//}
