6c6d6e28371753cbd47a09226d528a3048940eba
hiro-orbital <hiro.psp@hotmail.co.jp>
2017-07-28 22:01:56 +0900

    エリア判定関数追加

---
+++ app/src/main/java/com/example/tomoaki/maptest/AreaCheck.java
@@ -0,0 +1,24 @@
+package com.example.tomoaki.maptest;
+
+/**
+ * Created by hiro-orbital on 2017/07/28.
+ */
+
+public class AreaCheck {
+    public boolean AreaCheck(float ido, float keido){
+        boolean CheckResult;
+
+        //1つ目のエリア判定
+        if (ido < 36.326308 && ido >36.321588 && keido < 139.012157 && keido > 139.002510)
+           CheckResult = true;
+            else{
+            if (ido <36.3273144 && ido >36.326308 && keido <139.0068387 && keido >139.0066483)
+                CheckResult = true;
+            else
+                CheckResult = false;
+        }
+        return CheckResult;
+
+    }
+
+}
